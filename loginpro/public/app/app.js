f29e20199ef7cba1df5950f89b82156153639a29
Mohit Patil <bravoure546@gmail.com>
2015-09-20 14:38:48 +0530

    bootstrap and angular libraries added to project

--- loginpro/public/app/app.js
+++ loginpro/public/app/app.js
@@ -1,7 +1,7 @@
 // app.js
 // create our angular app and inject ngAnimate and ui-router
 // =============================================================================
-angular.module('formApp', ['formController','ngAnimate', 'ui.router'])
+angular.module('formApp', ['ui.router'])
 
 // configuring our routes
 // =============================================================================
@@ -12,7 +12,7 @@ angular.module('formApp', ['formController','ngAnimate', 'ui.router'])
         // route to show our basic form (/form)
         .state('form', {
             url: '/form',
-            templateUrl: 'form.html',
+            templateUrl: 'pages/form.html',
             controller: 'formController'
         })
 
@@ -21,22 +21,35 @@ angular.module('formApp', ['formController','ngAnimate', 'ui.router'])
         // url will be nested (/form/profile)
         .state('form.profile', {
             url: '/profile',
-            templateUrl: 'form-profile.html'
+            templateUrl: 'pages/form-profile.html'
         })
 
         // url will be /form/interests
         .state('form.interests', {
             url: '/interests',
-            templateUrl: 'form-interests.html'
+            templateUrl: 'pages/form-interests.html'
         })
 
         // url will be /form/payment
         .state('form.payment', {
             url: '/payment',
-            templateUrl: 'form-payment.html'
+            templateUrl: 'pages/form-payment.html'
         });
 
     // catch all route
     // send users to the form page
     $urlRouterProvider.otherwise('/form/profile');
 })
+
+.controller('formController', function($scope) {
+
+    // we will store all of our form data in this object
+    $scope.formData = {};
+
+    // function to process the form
+    $scope.processForm = function() {
+        alert('awesome!');
+    };
+
+});
+
---
+++ loginpro/public/app/static/js/app.js
@@ -0,0 +1,28 @@
+angular.module('OnlineAptitudeApp', ['OnlineAptitudeApp.controllers','ui.bootstrap','ui.router','OnlineAptitudeApp.services'])
+.config(function($stateProvider, $urlRouterProvider) {
+
+	$urlRouterProvider.otherwise('/test/categories');
+
+    $stateProvider
+    .state('home', {
+        url: '/home'
+    })
+
+    .state('fillQuestions', {
+        url: '/fill/questions',
+        templateUrl: 'fillQuestion.html',
+        controller: 'fillQuestionCtrl'
+    })
+
+     .state('testCategories', {
+        url: '/test/categories',
+        templateUrl: 'testCategories.html',
+        controller: 'testCategoriesCtrl'
+    });
+
+    // .state('testCategories', {
+    //     url: '/test/categories',
+    //     templateUrl: '/templates/admin/testCategories.html',
+    //     controller: 'testCategoriesCtrl'
+    // });
+});
